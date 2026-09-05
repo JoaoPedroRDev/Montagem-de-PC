49a01f2eecdf906efd7324e50b1ea611eb4723ff
João Pedro <jprodriguesx2@gmail.com>
2026-09-04 22:41:14 -0300

    Initial commit: Projeto Montador de PC

---
+++ JS/montador.js
@@ -0,0 +1,28 @@
+let BotaoQueCalcula = document.getElementById("botao")
+
+function CalculoTotal() {
+    let PrecoProcessador = Number(document.getElementById("processador").value);
+    let PrecoPlacaVideo = Number(document.getElementById("PlacaDeVideo").value);
+    let PrecoRam = Number(document.getElementById("MemoriaRam").value);
+    let QuantidadeMemoriaRam = Number(document.getElementById("QuantidadeRAM").value);
+    let PrecoArmazenamento = Number(document.getElementById("Armazenamento").value);
+
+    const TotalDaSoma = PrecoProcessador + PrecoPlacaVideo + (PrecoRam * QuantidadeMemoriaRam) + PrecoArmazenamento;
+
+    document.getElementById("resultado").innerHTML = "O seu PC custa: R$ " + TotalDaSoma
+
+    // Verificar se o "select" está com algum item/valor dentro dele
+    const seletores = document.getElementsByTagName("select")
+    for (let i = 0; i < seletores.length; i++) {
+        if (Number(seletores[i].value == 0)) {
+            seletores[i].style.border = '1px solid red'
+        } else {
+            seletores[i].style.border = '1px solid #f2bf07';
+        }
+    }
+}
+
+BotaoQueCalcula.addEventListener("click", CalculoTotal)
+
+
+// não mexer em nada.
